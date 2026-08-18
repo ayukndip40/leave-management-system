e830dd054f6fdcd89421f908b17783591ebaff95
Ayuk Ndip Elvis <ayukndip40@gmail.com>
2026-08-18 15:23:27 +0100

    Prepare app for Android and Railway deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ async function startServer() {
 
         connection.release();
 
-        app.listen(PORT, () => {
+        app.listen(PORT, "0.0.0.0", () => {
             console.log(`🚀 Server running on port ${PORT}`);
         });
 
